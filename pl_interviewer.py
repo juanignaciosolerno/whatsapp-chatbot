a1fa0bd2639c55f2087453e0a07f85f44c2af936
Juan Ignacio Solerno <135361987+juanignaciosolerno@users.noreply.github.com>
2024-09-08 19:14:13 -0300

    Setup a basic sms_reply

--- pl_interviewer.py
+++ pl_interviewer.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
+
+# Hello World endpoint
 @app.route("/")
 def hello():
   return "<p>This is Pensamiento Lateral ChatBot!</p>"
+
+
+# Respond to simple message endpoint
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Start our TwiML response
+    resp = MessagingResponse()
+    # Add a message
+    resp.message("The Robots are coming! Head for the hills!")
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 #if __name__ == "__main__":
 #  app.run()
