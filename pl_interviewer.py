a323c66033a964d7e7c5d6d78446b628edf013cc
Juan Ignacio Solerno <135361987+juanignaciosolerno@users.noreply.github.com>
2024-09-08 19:52:01 -0300

    Setup for authentication Twilio authentication

--- pl_interviewer.py
+++ pl_interviewer.py
@@ -1,8 +1,19 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, abort
 from twilio.twiml.messaging_response import MessagingResponse
+from twilio.request_validator import RequestValidator
+from functools import wraps
+
+import os
+
+from dotenv import load_dotenv
+
+# Load environmental variables from .env
+load_dotenv()
 
 app = Flask(__name__)
 
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+print(auth_token)
 
 # Hello World endpoint
 @app.route("/")
@@ -10,7 +21,7 @@ def hello():
   return "<p>This is Pensamiento Lateral ChatBot!</p>"
 
 
-# Respond to simple message endpoint
+# Respond with a simple message with MessagingResponse object 
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
     """Respond to incoming calls with a simple text message."""
@@ -20,8 +31,56 @@ def sms_reply():
     resp.message("The Robots are coming! Head for the hills!")
     return str(resp)
 
+
+# Validate Twilio Requests with a RequestValidator object
+def validate_twilio_request(f):
+    """Validates that incoming requests genuinely originated from Twilio"""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        # Create an instance of the RequestValidator class
+        validator = RequestValidator(os.environ.get('TWILIO_AUTH_TOKEN'))
+
+        # Validate the request using its URL, POST data,
+        # and X-TWILIO-SIGNATURE header
+        request_valid = validator.validate(
+            request.url,
+            request.form,
+            request.headers.get('X-TWILIO-SIGNATURE', ''))
+
+        # Continue processing the request if it's valid, return a 403 error if
+        # it's not
+        if request_valid:
+            return f(*args, **kwargs)
+        else:
+            return abort(403)
+    return decorated_function
+
+
+# Respond with a simple message with MessagingResponse object if it is a valid Twilio request
+@app.route('/message', methods=['POST'])
+@validate_twilio_request
+def incoming_message():
+    """Twilio Messaging URL - receives incoming messages from Twilio"""
+    # Create a new TwiML response
+    resp = MessagingResponse()
+
+    # <Message> a text back to the person who texted us
+    body = "Your text to me was {0} characters long. Webhooks are neat :)" \
+        .format(len(request.values['Body']))
+    resp.message(body)
+
+    # Return the TwiML
+    return str(resp)
+
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
-#if __name__ == "__main__":
-#  app.run()
+
+
+
+
+
