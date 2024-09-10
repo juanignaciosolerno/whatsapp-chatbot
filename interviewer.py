606ff5f01d2ee10c0ae952d380ab0e28f0e74032
Juan Ignacio Solerno <135361987+juanignaciosolerno@users.noreply.github.com>
2024-09-10 20:05:27 -0300

    simple_interview_setup

--- interviewer.py
+++ interviewer.py
@@ -83,8 +83,68 @@ def incoming_message():
     # Return the TwiML
     return str(resp)
 
+### ENTREVISTA
 
 
+# Preguntas predefinidas
+questions = [
+    "Hola, soy Vecinal, un asistente virtual. Te escribo porque estoy evaluando los servicios de la Muni en el barrio. ¿Cómo estás?",
+    "¿Tienes unos minutos para responder algunas preguntas sobre tu experiencia con los servicios de la Muni?",
+    "¿Del 1 al 10 cuan satisfecho/a estás con los servicios que ofrece la municipalidad en tu barrio?",
+    "¿Podrías decirme por qué le diste ese puntaje?",
+    "¿Del 1 al 10, cómo calificarías la limpieza y la recolección de residuos en el barrio?",
+    "¿Qué aspectos te parecen los más importantes a mejorar con respecto a esto último?",
+    "¿Del 1 al 10 cómo calificarías el estado de las calles y las veredas?",
+    "¿Qué aspectos te parecen los más importantes a mejorar con respecto a esto último?",
+    "¿Con respecto a la seguridad, hay algo que te preocupa o te parece importante mejorar?",
+    "¿En qué aspectos del barrio te parece que debería trabajarse?",
+    "¿Hay algo que esperas que la Municipalidad o el intendente haga en el barrio?",
+    "Te agradezco mucho por compartirme tus opiniones. ¿Hay algo más que te gustaría decirme?",
+    "Muchas gracias por tu tiempo y por ayudar a mejorar el barrio. ¡Que tengas un lindo día!"
+]
+
+
+# Función para obtener la pregunta actual
+def fetch_question(question_index):
+    if question_index < len(questions):
+        return questions[question_index]
+    else:
+        return 'Tu entrevista ya ha terminado, muchas gracias!'
+
+
+# Estado global (esto se debería manejar por sesión o en una base de datos para múltiples usuarios)
+status = {
+    "current_message": 0, 
+    "user_messages": {}
+}
+
+@app.route('/interview', methods=['POST'])
+def interview():
+    # Obtener el índice del mensaje actual
+    incoming_msg_index = status.get('current_message', 0)
+
+    # Revisar si la entrevista ya ha terminado
+    if incoming_msg_index > len(questions):
+        return "Tu entrevista ya ha terminado. Gracias nuevamente!"
+    
+    # Obtener el mensaje entrante del usuario
+    incoming_msg = request.values.get('Body', '').strip()
+
+    # Si el mensaje está vacío, devolver una respuesta de error
+    if not incoming_msg:
+        response_msg = 'Hola!. No encuentro ningún mensaje, me has querido decir algo?'
+        return response_msg
+
+    # Almacenar el mensaje del usuario
+    status["user_messages"][incoming_msg_index] = incoming_msg
+
+    # Obtener la siguiente pregunta
+    response_msg = fetch_question(incoming_msg_index)
+
+    # Incrementar el índice del mensaje actual
+    status["current_message"] += 1
+
+    return response_msg
 
 
 if __name__ == "__main__":
